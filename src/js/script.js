4fefd108e303dd0a8895dbff2d498177a9d4136f
aparan <aparan@example.com>
2021-12-23 22:47:25 +0400

    suggestions-alpha

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.suggestions__slicker').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/suggestions/left-arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/suggestions/next.svg"></button>'
+    });
+    $('.suggestions__discounts__slicker').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        infinite: false,
+        variableWidth: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/suggestions/left-arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/suggestions/next.svg"></button>'
+    });
+});
